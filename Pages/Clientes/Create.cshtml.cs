14f68f50409bdf28d8a0af4e4573a53190435503
Rafael <josuegonzazlz@gmail.com>
2025-04-24 11:44:15 -0500

    insert de clientes

--- Pages/Clientes/Create.cshtml.cs
+++ Pages/Clientes/Create.cshtml.cs
@@ -1,3 +1,5 @@
+using appweb1.Data;
+using appweb1.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,22 @@ namespace MyApp.Namespace
 {
     public class CreateModel : PageModel
     {
+        private readonly AppDbContext _ctx;
+        public CreateModel(AppDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+        [BindProperty]
+        public Cliente Cliente { get; set; }
         public void OnGet()
         {
         }
+        public IActionResult OnPost()
+        {
+            _ctx.Clientes.Add(Cliente);
+            _ctx.SaveChanges();
+            return RedirectToPage("Index");
+        }
+
     }
 }
